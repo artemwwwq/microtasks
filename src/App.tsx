4549bb8ffb8139516448d25e1d5fc900df7404df
artemwwwq <artem201190@mail.ru>
2023-02-05 00:53:38 +0500

    Microtasks Ассоциативный массив

--- src/App.tsx
+++ src/App.tsx
@@ -1,178 +1,105 @@
-import React, {MouseEvent, useState} from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from "./Component/FullInput";
-import {Input} from "./Component/input";
-import {Button} from "./Component/Button";
-//import {NewComponent} from "./Component/NewComponent";
-/*import {SwitchButton} from "./Component/switchButton";*/
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
+export type FilterValuesType = "all" | "active" | "completed";
 
-/*
-export type FilterType = 'ALL' | 'DOLLAR'| 'RUBLS'
-
-export type MoneyFilterType = {
-    banknots: string
-    value: number
-    number: string
+type todolistsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
 }
-
-export type currentMoneyType = {
-    currentMoney: Array<MoneyFilterType>
-}
-*/
-
 
 function App() {
 
-    /*const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const Button2Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const stupidButton = () => {
-        console.log('I am Stupid Button')
-    }
+    // let [tasks, setTasks] = useState([
+    //     {id: v1(), title: "HTML&CSS", isDone: true},
+    //     {id: v1(), title: "JS", isDone: true},
+    //     {id: v1(), title: "ReactJS", isDone: false},
+    //     {id: v1(), title: "Rest API", isDone: false},
+    //     {id: v1(), title: "GraphQL", isDone: false},
+    // ]);
+    // let [filter, setFilter] = useState<FilterValuesType>("all");
 
+    let todolistID1=v1();
+    let todolistID2=v1();
 
-
-    return (
-    <div className='App'>
-      <SwitchButton name={'YouTubeChanel-1'} callBack={()=>Button1Foo('i am Ivan', 25, 'I am living in Ekaterinburg')}/>
-      <SwitchButton name={'YouTubeChanel-2'} callBack={()=>Button2Foo('i am Artem', 23, 'I am living in Berezovsky')}/>
-      <SwitchButton name={'Stupid'} callBack={stupidButton}/>
-    </div>
-  );*/
-
-    /*let[a, setA] = useState(1)
-
-    const onClickHendler = () => {
-        setA(++a)
-        console.log(a)
-    }
-
-    const onClickDelete = () => {
-        setA(a = 0)
-    }
-
-    return (
-        <div className='App'>
-                <h1>{a}</h1>
-            <button onClick={onClickHendler} >number</button>
-            <button onClick={onClickDelete} >0</button>
-        </div>
-    )
-*/
-    {/* <ul>
-            {currentMoney.map((objFromMoneyArr, index)=>{
-                return (
-                    <li key={index}>
-                        <span>{objFromMoneyArr.banknots} </span>
-                        <span>{objFromMoneyArr.value}</span>
-                        <span>{objFromMoneyArr.number}</span>
-                    </li>
-                )
-            })}
-        </ul>
-            <div style={{marginLeft:'35px'}}>
-            <button onClick={()=>onClickFilterHandler('ALL')} >ALL</button>
-            <button onClick={()=>onClickFilterHandler('RUBLS')}>RUBLES</button>
-            <button onClick={()=>onClickFilterHandler('DOLLAR')}>DOLLARS</button>
-            </div>*/}
-
-
-    /*const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    let [todolists, setTodolists] = useState<Array<todolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
 
-    const [Filter, setFilter] = useState<FilterType>('ALL')
 
 
-    let currentMoney = money;
-
-    if(Filter==='DOLLAR') {
-        currentMoney = money.filter((filteredMoney)=>filteredMoney.banknots==='Dollars')
+    function removeTask(todolistID: string, id: string) {
+        setTasks({...tasks, [todolistID]:tasks[todolistID].filter(el=>el.id !== id)})
     }
-    if(Filter==='RUBLS') {
-        currentMoney = money.filter((filteredMoney)=>filteredMoney.banknots==='RUBLS')
+
+    function addTask(todolistID:string, title: string) {
+        let newTask = {id:v1(), title: title, isDone: false}
+        setTasks({...tasks, [todolistID]: [ newTask,...tasks[todolistID]]})
     }
 
-        const onClickFilterHandler = (nameButton: FilterType) => {
-            setFilter(nameButton)
-        }
+    function changeStatus(todolistID:string ,taskId: string, NewIsDone: boolean) {
+        setTasks({...tasks, [todolistID]: tasks[todolistID].map(el=> el.id === taskId ? {...el, isDone:NewIsDone } : el)})
+    }
+
+
+
+
+    function changeFilter(todolistID: string, value: FilterValuesType) {
+        setTodolists(todolists.map(filtered=> filtered.id === todolistID ? {...filtered, filter: value} : filtered))
+    }
+
 
     return (
-        <>
-            <NewComponent currentMoney={currentMoney}
-                          onClickFilterHandler={onClickFilterHandler}
-            />*/
-           {/* <ul>
-            {currentMoney.map((objFromMoneyArr, index)=>{
+        <div className="App">
+            {
+                todolists.map(todolist=> {
+
+                    let tasksForTodolist = tasks[todolist.id];
+
+                    if (todolist.filter === "active") {
+
+                        tasksForTodolist = tasks[todolist.id].filter(t => !t.isDone);
+                    }
+                    if (todolist.filter === "completed") {
+                        tasksForTodolist = tasks[todolist.id].filter(t => t.isDone);
+                    }
+
                 return (
-                    <li key={index}>
-                        <span>{objFromMoneyArr.banknots} </span>
-                        <span>{objFromMoneyArr.value}</span>
-                        <span>{objFromMoneyArr.number}</span>
-                    </li>
-                )
-            })}
-        </ul>
-            <div style={{marginLeft:'35px'}}>
-            <button onClick={()=>onClickFilterHandler('ALL')} >ALL</button>
-            <button onClick={()=>onClickFilterHandler('RUBLS')}>RUBLES</button>
-            <button onClick={()=>onClickFilterHandler('DOLLAR')}>DOLLARS</button>
-            </div>*/}
-       /* </>*/
+                <Todolist key={todolist.id}
+                          todolistID={todolist.id}
+                          title={todolist.title}
+                          tasks={tasksForTodolist}
+                          removeTask={removeTask}
+                          changeFilter={changeFilter}
+                          addTask={addTask}
+                          changeTaskStatus={changeStatus}
+                          filter={todolist.filter}
+                />
+                )})}
 
-
-   /* )*/
-
-
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'}
-    ])
-
-
-    let [title, setTitle] = useState('')
-
-
-
-
-    const addMessage =(title: string)=> {
-        let newMessage = {message: title}
-        setMessage([newMessage, ...message])
-    }
-
-    const callBackButtonHandler = ()=> {
-        addMessage(title)
-        setTitle('')
-    }
-    return (
-        <div className={'App'}>
-            {/*<FullInput addMessage={addMessage}/>*/}
-            <Input setTitle={setTitle} title={title}/>
-            <Button name={'+'} callBack={callBackButtonHandler}/>
-            {message.map((el, index)=>{
-                return (
-
-                    <div key={index}>{el.message}</div>
-                )
-            })}
         </div>
-    )
+    );
 }
-
-
 
 export default App;
